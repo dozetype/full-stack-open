912580ddf68b799d1d8f0ab0f170ff8e40d03721
xw <johntheghostie@gmail.com>
2025-07-15 20:10:35 +0800

    2.6 - 2.10 PhoneBook

---
+++ part2/phonebook/src/components/Filter.jsx
@@ -0,0 +1,10 @@
+const Filter = ({filter, fn}) => {
+    return(
+        <div>
+            filter shown with{" "}
+            <input value={filter} onChange={fn} />
+        </div>
+    )
+}
+
+export default Filter;
---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,13 @@
+const Persons = ({peopleToShow}) =>{
+    return (
+        <div>
+            {peopleToShow.map((person) => (
+                <p key={person.id}>
+                    {person.name} {person.number}
+                </p>
+            ))}
+        </div>
+    );
+}
+
+export default Persons;
