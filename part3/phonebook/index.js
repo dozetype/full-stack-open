2064c12e19f4e373b28614fa34039736220a1736
xw <johntheghostie@gmail.com>
2025-07-20 00:45:41 +0800

    3.13 - 3.18 PhoneBook
    Now using mongo Atlus DB instead of local
    Able to do most of the CRUD commands
    Also has some error handling

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
-let persons = require("./persons");
-let morgan = require('morgan')
+let morgan = require("morgan");
+const Person = require("./models/person");
 const app = express();
 // const requestLogger = (request, response, next) => {
 //     console.log("Method:", request.method);
@@ -9,63 +10,118 @@ const app = express();
 //     console.log("---");
 //     next();
 // };
-app.use(express.static('dist')) //Allow Express to run frontend code, allowing backend to serve full stack
+app.use(express.static("dist")); //Allow Express to run frontend code, allowing backend to serve full stack
 app.use(express.json());
-app.use(morgan('tiny'))
-// app.use(requestLogger);
+app.use(morgan("tiny"));
 
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons);
+    Person.find({}).then((p) => res.json(p));
 });
 
 app.get("/info", (req, res) => {
-    res.send(
-        `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`,
-    );
+    Person.countDocuments({}).then((count) => {
+        res.send(
+            `<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`,
+        );
+    });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-    const id = req.params.id;
-    const p = persons.find((p) => p.id === id);
-    if (p) res.json(p);
-    else res.status(404).end();
+app.get("/api/persons/:id", (req, res, next) => {
+    Person.findById(req.params.id)
+        .then((p) => {
+            if (p) {
+                res.json(p);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-    const id = req.params.id;
-    persons = persons.filter((p) => p.id !== id);
-    res.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then((result) => {
+            response.status(204).end();
+        })
+        .catch((error) => next(error));
 });
+
+// app.post("/api/persons", (req, res) => {
+//     // TODO need to learn await and async for counter
+//     const body = req.body;
+//     if (!body.name || !body.number)
+//         return res.status(404).json({ error: "The name or number is missing" });
+//     if (Person.find((p) => p.name === body.name))
+//         return res
+//             .status(404)
+//             .json({ error: "The name already exists in the phonebook" });
+//     const p = {
+//         id: Math.floor(Math.random() * 1000).toString(),
+//         name: body.name,
+//         number: body.number,
+//     };
+//     console.log(body);
+//     persons = persons.concat(p);
+//     res.json(p);
+// });
 
 app.post("/api/persons", (req, res) => {
-    const body = req.body;
-    if (!body.name || !body.number)
-        return res.status(404).json({ error: "The name or number is missing" });
-    if (persons.find((p) => p.name === body.name))
-        return res
-            .status(404)
-            .json({ error: "The name already exists in the phonebook" });
-    const p = {
-        id: Math.floor(Math.random() * 1000).toString(),
-        name: body.name,
-        number: body.number,
-    };
-    console.log(body);
-    persons = persons.concat(p);
-    res.json(p);
+    const { name, number } = req.body;
+
+    if (!name || !number) { //doesnt work
+        return res.status(400).json({ error: "content missing" });
+    }
+    
+    const person = new Person({
+        name: name,
+        number: number,
+    });
+
+    person.save().then((savedP) => {
+        res.json(savedP);
+    });
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+    const { name, number } = req.body;
+
+    Person.findById(req.params.id)
+        .then((p) => {
+            if (!p) {
+                return res.status(404).end();
+            }
+
+            // p.name = name;
+            p.number = number;
+
+            return p.save().then((updatedPerson) => {
+                res.json(updatedPerson);
+            });
+        })
+        .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+    response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
 
-app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
 
-const PORT = process.env.PORT || 3001 //check if environment PORT exists
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    }
+
+    next(error);
+};
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001; //check if environment PORT exists
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
