ee3cd2fb4ef13ab5eb16a015ff93ae68e0811d5a
xw <johntheghostie@gmail.com>
2025-08-01 14:22:15 +0800

    Added helper file, cleaned code

--- part5/playwright/playwright.config.js
+++ part5/playwright/playwright.config.js
@@ -1,17 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 export default defineConfig({
     testDir: "./tests",
     /* Run tests in files in parallel */
@@ -27,7 +16,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://localhost:3000',
+        baseURL: 'http://localhost:5173',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,16 +1,20 @@
+// USE npm run start:test on backend, USE npm run dev on frontend
+
 const { test, expect, describe, beforeEach } = require("@playwright/test");
+const { loginWith, createBlog } = require("./helper");
+const { create } = require("node:domain");
 
 describe("Blog App", () => {
     beforeEach(async ({ page, request }) => {
-        await request.post("http://localhost:3001/api/testing/reset");
-        await request.post("http://localhost:3001/api/users", {
+        await request.post("/api/testing/reset"); // baseURL is localhost:5173
+        await request.post("/api/users", {
             data: {
                 name: "USER2 NAME",
                 username: "user2",
                 password: "password",
             },
         });
-        await page.goto("http://localhost:5173");
+        await page.goto("/");
     });
     test("front page can be opened", async ({ page }) => {
         const locator = await page.getByText("Log in to application");
@@ -18,40 +22,35 @@ describe("Blog App", () => {
     });
 
     test("login form can be opened", async ({ page }) => {
-        await page.getByTestId("username").fill("user2");
-        await page.getByTestId("password").fill("password");
-        await page.getByRole("button", { name: "login" }).click();
+        loginWith(page, "user2", "password");
         await expect(page.getByText("user2 logged in")).toBeVisible();
     });
 
     test("login fails with wrong password", async ({ page }) => {
-        await page.getByTestId("username").fill("dozetype");
-        await page.getByTestId("password").fill("wrong password");
-        await page.getByRole("button", { name: "login" }).click();
+        await loginWith(page, "dozetype", "wrong password");
 
         const errorDiv = page.locator(".error"); //locating user css class
         await expect(errorDiv).toContainText("Wrong Username or Password");
         await expect(errorDiv).toHaveCSS("border-style", "solid");
         await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
-          await expect(page.getByText('dozetype logged in')).not.toBeVisible()
+        await expect(page.getByText("dozetype logged in")).not.toBeVisible();
     });
 
     describe("when logged in", () => {
         beforeEach(async ({ page }) => {
-            await page.getByTestId("username").fill("user2");
-            await page.getByTestId("password").fill("password");
-            await page.getByRole("button", { name: "login" }).click();
+            await loginWith(page, "user2", "password");
         });
 
         test("a new note can be created", async ({ page }) => {
-            await page.getByRole("button", { name: "new blog" }).click();
-            await page.getByTestId("Title").fill("playwright test");
-            await page.getByTestId("Author").fill("playwright test2");
-            await page.getByTestId("URL").fill("playwright test3");
-            await page.getByRole("button", { name: "create" }).click();
+            await createBlog(
+                page,
+                "playwright title",
+                "playwright author",
+                "playwright url",
+            );
             await expect(
                 page.getByText(
-                    "a new blog playwright test by playwright test2 added",
+                    "a new blog playwright title by playwright author added",
                 ),
             ).toBeVisible();
         });
